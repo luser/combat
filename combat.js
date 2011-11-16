552073c0f9653b9b84126e272b410b2a78770e29
Ted Mielczarek <ted@mielczarek.org>
2011-11-16 18:38:01 -0500

    initial import

---
+++ combat.js
@@ -0,0 +1,261 @@
+window.requestAnimFrame = (function(){
+          return  window.requestAnimationFrame       || 
+                  window.webkitRequestAnimationFrame || 
+                  window.mozRequestAnimationFrame    || 
+                  window.oRequestAnimationFrame      || 
+                  window.msRequestAnimationFrame     || 
+                  function(/* function */ callback, /* DOMElement */ element){
+                    window.setTimeout(callback, 1000 / 60);
+                  };
+    })();
+
+// box2d world state
+var world;
+// game state
+var game;
+// rendering backend
+var renderer;
+var players = [];
+var SCALE = 30;
+var WORLD_WIDTH = 640;
+var WORLD_HEIGHT = 480;
+var PLAYER_ACCEL = 10;
+var MAX_SPEED = 5;
+var FIRE_COOLDOWN = 500; // milliseconds
+
+var   b2Vec2 = Box2D.Common.Math.b2Vec2
+,  b2AABB = Box2D.Collision.b2AABB
+,	b2BodyDef = Box2D.Dynamics.b2BodyDef
+,	b2Body = Box2D.Dynamics.b2Body
+,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+,	b2Fixture = Box2D.Dynamics.b2Fixture
+,	b2World = Box2D.Dynamics.b2World
+,	b2MassData = Box2D.Collision.Shapes.b2MassData
+,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+,  b2MouseJointDef =  Box2D.Dynamics.Joints.b2MouseJointDef
+;
+
+function Game(world) {
+  this.world = world;
+  this.players = [];
+  this.blocks = [];
+}
+
+Game.prototype = {
+  addStaticBox: function addStaticBox(opts) {
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.2;
+
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_staticBody;
+
+    var x = opts.x || 0;
+    var y = opts.y || 0;
+    var width = opts.width | 10;
+    var height = opts.height | 10;
+    // positions the center of the object (not upper left!)
+    bodyDef.position.x = (x + width/2) / SCALE;
+    bodyDef.position.y = (y + height/2) / SCALE;
+
+    fixDef.shape = new b2PolygonShape;
+    fixDef.shape.SetAsBox(width / SCALE, height / SCALE);
+    this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+    this.blocks.push(new Block(x, y, width, height));
+  },
+
+  addWalls: function addWalls(width, height) {
+    // top
+    this.addStaticBox({width: width, height: 3, x: 0, y: 0});
+    // bottom
+    this.addStaticBox({width: width, height: 3, x: 0, y: height-3});
+    // left
+    this.addStaticBox({width: 3, height: height, x: 0, y: 0});
+    // right
+    this.addStaticBox({width: 3, height: height, x: width-3, y: 0});
+  },
+
+  addPlayer: function addPlayer() {
+    var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.2;
+
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_dynamicBody;
+    fixDef.shape = new b2CircleShape(
+      0.5 //radius
+    );
+    bodyDef.position.x = Math.random() * 25;
+    bodyDef.position.y = Math.random() * 10;
+    bodyDef.linearDamping = 10.0;
+    bodyDef.angularDamping = Infinity;
+    var body = world.CreateBody(bodyDef);
+    var player = new Player(body);
+    body.CreateFixture(fixDef);
+    body.SetUserData(player);
+    this.players.push(player);
+    return player;
+  },
+
+  run: function run() {
+    for (var i = 0; i < this.players.length; i++) {
+      this.players[i].applyInput();
+    }
+    this.world.Step(
+      1 / 60   //frame-rate
+      ,  10       //velocity iterations
+      ,  10       //position iterations
+    );
+    this.world.ClearForces();
+
+    for (i = 0; i < this.players.length; i++) {
+      this.players[i].syncPosition();
+    }
+  },
+
+  keyInput: function keyInput(inputName, pressed) {
+    //TODO: non-hardcoded
+    this.players[0].keyState[inputName] = pressed;
+  }
+};
+
+function Player(body) {
+  this.body = body;
+  this.lastFire = 0;
+  this.keyState = {'forward': false,
+                  'back': false,
+                  'left': false,
+                  'right': false,
+                  'fire': false};
+  this.color = "#ff0000";
+  this.syncPosition();
+}
+
+Player.prototype = {
+  getSpeed: function getSpeed() {
+    return this.body.GetLinearVelocity().Length();
+  },
+
+  getAngleVec: function getAngleVec(length) {
+    var a = this.body.GetAngle();
+    var v = new b2Vec2(Math.cos(a) * length,
+                       Math.sin(a) * length);
+    return v;
+  },
+
+  applyInput: function applyInput() {
+    var speed = this.getSpeed();
+    if (this.keyState['forward'] && speed < MAX_SPEED) {
+      this.body.ApplyForce(this.getAngleVec(PLAYER_ACCEL),
+                           this.body.GetWorldCenter());
+    }
+    else if (this.keyState['back'] && speed < MAX_SPEED) {
+      this.body.ApplyForce(this.getAngleVec(-1 * PLAYER_ACCEL),
+                           this.body.GetWorldCenter());
+    }
+    if (this.keyState['left']) {
+      this.body.SetAngle(this.body.GetAngle() - Math.PI/360);
+    }
+    else if (this.keyState['right']) {
+      this.body.SetAngle(this.body.GetAngle() + Math.PI/360);
+    }
+
+    if (this.keyState['fire']) {
+      var now = Date.now();
+      if (this.lastFire + FIRE_COOLDOWN < now) {
+        this.lastFire = now;
+        this.fire();
+      }
+    }
+  },
+
+  syncPosition: function syncPosition() {
+    var d = this.body.GetDefinition();
+    this.x = d.position.x * SCALE;
+    this.y = d.position.y * SCALE;
+  },
+
+  fire: function() {
+    spawnBullet(this);
+  }
+};
+
+function Block(x, y, width, height, color) {
+  this.x = x;
+  this.y = y;
+  this.width = width;
+  this.height = height;
+  this.color = color || "#000000";
+};
+
+
+function spawnBullet(player) {
+  var fixDef = new b2FixtureDef;
+  fixDef.density = 1.0;
+  fixDef.friction = 0.5;
+  fixDef.restitution = 0.2;
+
+  var bodyDef = new b2BodyDef;
+  bodyDef.type = b2Body.b2_kinematicBody;
+  bodyDef.bullet = true;
+  fixDef.shape = new b2CircleShape(0.1);
+
+  // Spawn just outside the player in the angle they're facing
+  var pos = player.body.GetWorldCenter().Copy();
+  var size = player.body.GetFixtureList().GetShape().GetRadius();
+  var vec = player.getAngleVec(size + 0.1);
+  pos.Add(vec);
+  bodyDef.position.x = pos.x;
+  bodyDef.position.y = pos.y;
+  var body = world.CreateBody(bodyDef);
+  body.CreateFixture(fixDef);
+  body.SetLinearVelocity(player.getAngleVec(10));
+}
+
+function init() {
+  world = new b2World(
+    new b2Vec2(0, 0)    // no gravity
+    ,  true                 //allow sleep
+  );
+  game = new Game(world);
+
+  game.addWalls(WORLD_WIDTH, WORLD_HEIGHT);
+
+  for (var i = 0; i < 5; i++) {
+    game.addStaticBox({width: 30, height: 30, x: Math.floor(Math.random()*(WORLD_WIDTH - 30)), y: Math.floor(Math.random()*(WORLD_HEIGHT - 30))});
+  }
+  game.addPlayer();
+
+  renderer =
+    //new DebugRenderer(world, WORLD_WIDTH, WORLD_HEIGHT);
+  new Canvas2DRenderer(world, WORLD_WIDTH, WORLD_HEIGHT);
+
+  requestAnimFrame(update);
+}
+
+function update() {
+  game.run();
+  renderer.render(game);
+  requestAnimFrame(update);
+}
+
+function keyChange(ev) {
+  var maps = {
+    32: 'fire',
+    37: 'left',
+    38: 'forward',
+    39: 'right',
+    40: 'back'
+  };
+  if (ev.keyCode in maps) {
+    game.keyInput(maps[ev.keyCode], ev.type == "keydown");
+  }
+}
+
+window.addEventListener("load", init, true);
+window.addEventListener("keydown", keyChange, true);
+window.addEventListener("keyup", keyChange, true);
---
+++ render.js
@@ -0,0 +1,49 @@
+// foo
+function DebugRenderer(b2world, width, height) {
+  var d = document.createElement("div");
+  d.innerHTML = '<canvas width="'+width+'" height="'+height+'" style="background-color:#333333;"></canvas>';
+  this.canvas = d.firstChild;
+  document.body.appendChild(d);
+  this.b2world = b2world;
+
+  var debugDraw = new b2DebugDraw();
+  debugDraw.SetSprite(this.canvas.getContext("2d"));
+  debugDraw.SetDrawScale(SCALE);
+  debugDraw.SetFillAlpha(0.3);
+  debugDraw.SetLineThickness(1.0);
+  debugDraw.SetFlags(b2DebugDraw.e_shapeBit | b2DebugDraw.e_jointBit);
+  b2world.SetDebugDraw(debugDraw);
+}
+
+DebugRenderer.prototype = {
+  render: function(game) {
+    this.b2world.DrawDebugData();
+  }
+};
+
+function Canvas2DRenderer(b2world, width, height) {
+  this.width = width;
+  this.height = height;
+  var d = document.createElement("div");
+  d.innerHTML = '<canvas width="'+width+'" height="'+height+'" style="background-color:#FFFFFF;"></canvas>';
+  this.canvas = d.firstChild;
+  this.ctx = this.canvas.getContext("2d");
+  document.body.appendChild(d);
+}
+
+Canvas2DRenderer.prototype = {
+  render: function(game) {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    for (var i = 0; i < game.blocks.length; i++) {
+      var b = game.blocks[i];
+      this.ctx.fillStyle = b.color;
+      this.ctx.fillRect(b.x, b.y, b.width, b.height);
+    }
+
+    for (i = 0; i < game.players.length; i++) {
+      var p = game.players[i];
+      this.ctx.fillStyle = p.color;
+      this.ctx.fillRect(p.x, p.y, 10, 10);
+    }
+  }
+};
