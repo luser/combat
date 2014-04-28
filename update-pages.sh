0993a1638ffb0e2a761695ffe8a81b03a1a92a1d
Ted Mielczarek <ted@mielczarek.org>
2014-04-28 13:25:40 -0400

    update from master

--- update-pages.sh
+++ update-pages.sh
@@ -2,6 +2,6 @@
 # Update example code on gh-pages branch.
 
 git checkout gh-pages
-git checkout master -- *.js index.html font sound style.css
+git checkout master -- *.js index.html font sound style.css *.png
 git commit -am "update from master"
 git checkout master
